6c479c82013ec7ff92f83a375d0f15a58a134d6a
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-22 10:18:43 +0000

    .

--- app/src/components/MoviesList.js
+++ app/src/components/MoviesList.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function MoviesList() {
+  const [films, setFilms] = useState([]);
+  
+  useEffect(() => {
+    axios.get('https://ghibliapi.herokuapp.com/films/')
+      .then(res => {
+        console.log(res.data);
+        setFilms(res.data);
+      })
+      .catch(err => {
+        console.log('The data was not returned', err);
+      })
+  }, []);
+  
 	return (
 		<div className="movies-container">
 			
