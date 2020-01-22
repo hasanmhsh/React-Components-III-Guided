c5e4c8976060199f9e6a43c8c76ae9d7797891ec
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-22 09:44:08 +0000

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './App.css';
 
 import MoviesList from './components/MoviesList';
 // import MouseMoveContainer from './components/MouseMoveContainer';
 
-function App() {
+function App(props) {
+
+  useEffect(() => {
+    // When the component mounts (after rendering to the DOM)
+    // Side effects live here
+    console.log('App component mounted.');
+    
+    return () => {
+      // When the component will unmount (just before component is removed from the DOM)
+      // Any final side effects will live here
+      console.log('App component unmounting...');
+    }
+  }, [
+    // Variables that we want to watch for changes
+  ]);
+
 	return (
 		<div className="App">
 			<header>
