8ee733952f5895e6917dfc518b2f3074e3c917c4
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-22 10:48:37 +0000

    .

--- app/src/components/MoviesList.js
+++ app/src/components/MoviesList.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 function MoviesList() {
   const [films, setFilms] = useState([]);
+  const [people, setPeople] = useState([]);
   
   useEffect(() => {
     axios.get('https://ghibliapi.herokuapp.com/films/')
@@ -15,10 +17,33 @@ function MoviesList() {
       })
   }, []);
   // ☝️ The empty dependency array prevents infinite loops
-		// It fires the effect hook on the first component mount
+    // It fires the effect hook on the first component mount
+    
+    // Hester
+  useEffect(() => {
+    axios
+        .get('https://ghibliapi.herokuapp.com/people/')
+        .then(response => {
+            console.log('people', response.data);
+            setPeople(response.data);
+        })
+        .catch(error => {
+            console.log('error', error);
+        });
+    }, []);
 	return (
 		<div className="movies-container">
-			
+      {
+        films.map(film => {       
+          return <MovieCard
+            key={film.id}
+            description={film.description}
+            title={film.title}
+            release_date={film.release_date}
+            director={film.director}
+          />
+        })
+      }
 		</div>
 	);
 }
